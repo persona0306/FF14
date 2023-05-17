bc47ba95e4bbfc2e60f0926e6b1129f8a7060458
potet <potet@192.168.11.13>
2023-05-17 18:18:03 +0900

    /jobchangeジョブチェンジできなかった場合joblistを引っ張ってくるようになった

--- src/potato/ball/ff14/Command_JobChange.java
+++ src/potato/ball/ff14/Command_JobChange.java
@@ -21,22 +21,32 @@ public class Command_JobChange implements CommandExecutor {
 		Player player = (Player)sender;
 		
 		Job job;
-		if (args[0].equalsIgnoreCase("Knight")) {
+		if (args[0].equalsIgnoreCase("Knight") ||
+			args[0].equalsIgnoreCase("K")) {
 			job = new Knight(player);
+			
 			sender.sendMessage("ジョブを"+ChatColor.BLUE+ChatColor.BOLD+"Knight"+
 								ChatColor.WHITE+"に変更しました");
-		}else if (args[0].equalsIgnoreCase("MagicDoctor")) {
+			
+		}else if (args[0].equalsIgnoreCase("MagicDoctor") ||
+				  args[0].equalsIgnoreCase("MD")) {
 			job = new MagicDoctor(player);
+			
 			sender.sendMessage("ジョブを"+ChatColor.LIGHT_PURPLE+ChatColor.BOLD+"Magic"+
 								ChatColor.GREEN+ChatColor.BOLD+"Doctor"+
 								ChatColor.WHITE+"に変更しました");
-		}else if (args[0].equalsIgnoreCase("UndeadHunter")) {
+			
+		}else if (args[0].equalsIgnoreCase("UndeadHunter")||
+				  args[0].equalsIgnoreCase("UH")) {
 			job = new UndeadHunter(player);
+			
 			sender.sendMessage("ジョブを"+ChatColor.DARK_PURPLE+ChatColor.BOLD+"Undead"+
 								ChatColor.GOLD+ChatColor.BOLD+"Hunter"+
 								ChatColor.WHITE+"に変更しました");
+			
 		}else {
 			sender.sendMessage("そのジョブは存在しません");
+			player.performCommand("jl");
 			
 			return true;
 		}
